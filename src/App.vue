a1f1d42f9ab8e3f6486f8030ce0a04ec633c5991
Adam <adamorlowskipoland@icloud.com>
2018-04-02 13:51:36 +0200

    init

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-app>
+
+    <v-toolbar fixed app>
+      <v-toolbar-title>Snackbar Demo</v-toolbar-title>
+    </v-toolbar>
+
+    <v-content>
+      <v-container fluid>
+        <v-slide-y-transition mode="out-in">
+          <v-layout column align-center>
+
+            <router-view></router-view>
+
+          </v-layout>
+        </v-slide-y-transition>
+      </v-container>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data () {
+  },
+  name: 'App'
+}
+</script>
